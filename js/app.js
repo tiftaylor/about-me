29b580a21aab21b545e8ddd2c1fc2bcacd3bf546
Tif Taylor <tiftaylorisrad@gmail.com>
2020-07-07 16:40:32 -0700

    Added | Code to JS file

--- js/app.js
+++ js/app.js
@@ -1,22 +1,63 @@
 'use strict';
 
-/* Instructions: 
-  - 5 yes/no Questions
-  - Input for answer must be either: y, n, yes, no (include catching case) .toLowerCase()
-  - Add alert reply each time they answer correctly
-  - Add console.log() to notify correct / then comment out & Add alerts()
-  - Ask the user their name
-  - Make a greeting alert using name
-  - Make a final message using name
-*/
-
 // What is userName + Custom Greeting passing in userName
 var userName = prompt('Hello, visitor! What is your name?');
-alert('Welcome ' + userName + '! I\'m going to ask you a series of questions about Tif Taylor. Please use \'y\'/\'yes\' or \'n\'/\'no\' when answering.');
+alert('Welcome ' + userName + '! I\'m going to ask you a series of questions about Tif Taylor. Please use \'yes\' or \'no\' when answering.');
 
 // 5 Question Series | Yes/No
-// Q1 - Is Tif based in Seattle? (yes or no)
-// Q2 - Does Tif social dance? (yes or no)
-// Q3 - Does Tif have a degree in Philosophy? (yes or no)
-// Q4 - Does Tif own a cat? (yes or no)
-// Q5 - Does Tif want to write and publish a book? (yes or no)
+var currentCity = prompt('Is Tif based in Seattle? (yes or no)').toLowerCase();
+if(currentCity === 'y' || currentCity === 'yes') {
+  alert('Correct! Seattle is lovely! (usually...)');
+  // console.log('Is Tif based in Seattle? ' + currentCity + ' is correct');
+} else if(currentCity === 'n' || currentCity === 'no') {
+  alert('Nope...try again.');
+  // console.log('Is Tif based in Seattle? ' + currentCity + ' is incorrect');
+} else {
+  alert('Please enter yes or no');
+}
+
+var doDance = prompt('Does Tif social dance? (yes or no)').toLowerCase();
+if(doDance === 'y' || doDance === 'yes') {
+  alert('I sure do! I swing, blues and fusion dance!');
+  // console.log('Does Tif social dance? ' + doDance + ' is correct');
+} else if(doDance === 'n' || doDance === 'no') {
+  alert('Incorrect - It may not seem like it on Zoom but I got mooooves!');
+  // console.log('Does Tif social dance? ' + doDance + ' is incorrect');
+} else {
+  alert('Please enter yes or no');
+}
+
+var philoDegree = prompt('Does Tif have a degree in Philosophy? (yes or no)').toLowerCase();
+if(philoDegree === 'y' || philoDegree === 'yes') {
+  alert('Yeah...I am in a constant state of existentialism, LOL');
+  // console.log('Does Tif have a degree in Philosophy? ' + philoDegree + ' is correct');
+} else if(philoDegree === 'n' || philoDegree === 'no') {
+  alert('Sadly, you\'re wrong. I majored in Philosophy of the Mind');
+  // console.log('Does Tif have a degree in Philosophy? ' + philoDegree + ' is incorrect');
+} else {
+  alert('Please enter yes or no');
+}
+
+var ownCat = prompt('Does Tif own a cat? (yes or no)').toLowerCase();
+if(ownCat === 'y' || ownCat === 'yes') {
+  alert('Heck no fren! Doggos only plz.');
+  // console.log('Does Tif own a cat? ' + ownCat + ' is incorrect');
+} else if(ownCat === 'n' || ownCat === 'no') {
+  alert('Doin me an excite friendo! Cats are lame');
+  // console.log('Does Tif own a cat? ' + ownCat + ' is correct');
+} else {
+  alert('Please enter yes or no');
+}
+
+var publishBook = prompt('Does Tif want to write and publish a book? (yes or no)').toLowerCase();
+if(publishBook === 'y' || publishBook === 'yes') {
+  alert('Correct! I do, but it\'s time consuming!');
+  // console.log('Does Tif want to write and publish a book? ' + publishBook + ' is correct');
+} else if(publishBook === 'n' || publishBook === 'no') {
+  alert('Incorrect, my friend. I aspire to be remembered forever!');
+  // console.log('Does Tif want to write and publish a book? ' + publishBook + ' is incorrect');
+} else {
+  alert('Please enter yes or no');
+}
+
+alert('Thanks for taking the time to try and get to know me, ' + userName + ':)');
